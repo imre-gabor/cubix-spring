a5247cf096fe6218e19f16cfe5a9c676f97860b3
imre-gabor <gabor@aut.bme.hu>
2023-04-07 12:10:48 +0200

    add simple controller class and Thymeleaf page

---
+++ airport/src/main/java/hu/cubix/airport/controller/AirportController.java
@@ -0,0 +1,36 @@
+package hu.cubix.airport.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hu.cubix.airport.dto.AirportDto;
+
+@Controller
+public class AirportController {
+
+	private List<AirportDto> airports = new ArrayList<>();
+	
+	{
+		airports.add(new AirportDto(1, "Budapest Ferenc Liszt International", "BUD"));
+	}
+	
+
+	@GetMapping("/")
+	public String home(Map<String, Object> model) {
+		model.put("airports", airports);
+		model.put("newAirport", new AirportDto());
+		return "index";
+	}
+	
+	
+	@PostMapping("/airport")
+	public String createAirport(AirportDto airport) {
+		airports.add(airport);
+		return "redirect:/";
+	}
+}
---
+++ airport/src/main/java/hu/cubix/airport/dto/AirportDto.java
@@ -0,0 +1,39 @@
+package hu.cubix.airport.dto;
+
+public class AirportDto {
+
+	private long id;
+	private String name;
+	private String iata;
+	
+	public AirportDto() {
+	}
+	
+	public AirportDto(long id, String name, String iata) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.iata = iata;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIata() {
+		return iata;
+	}
+	public void setIata(String iata) {
+		this.iata = iata;
+	}
+	
+	
+}
